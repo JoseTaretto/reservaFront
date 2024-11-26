8e4c30fe55520332a8902bd92cf4db8d9fa7693c
TeemoPseudoDev <jose.martinez.taretto@gmail.com>
2024-11-26 18:09:42 -0300

    fetch a api local

--- src/app/components/local-form/local-form.component.ts
+++ src/app/components/local-form/local-form.component.ts
@@ -6,22 +6,44 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './local-form.component.html',
-  styleUrl: './local-form.component.css'
+  styleUrls: ['./local-form.component.css'] // Cambiado a 'styleUrls'
 })
 export class LocalFormComponent {
+  formNegocio: FormGroup;
 
-formNegocio: FormGroup;
-
-  constructor (private form: FormBuilder){
-    this.formNegocio = this.form.group({
-      name:['', Validators.required],
-      horario:['',Validators.required],
-      img: ['']
-    })
+  constructor(private formBuilder: FormBuilder) {
+    this.formNegocio = this.formBuilder.group({
+      name: ['', Validators.required],
+      horario: ['', Validators.required],
+      img: [''],
+    });
   }
 
   onSubmit() {
-  console.log(this.formNegocio);
-  }
+    if (this.formNegocio.valid) {
+      const formData = this.formNegocio.value; // Obtén los valores del formulario
 
+      fetch('http://localhost:3000/routerNegocio', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData), // Envía los datos del formulario como JSON
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Error al enviar los datos al servidor');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log('Respuesta del servidor:', data);
+        })
+        .catch(error => {
+          console.error('Error al enviar el formulario:', error);
+        });
+    } else {
+      console.error('El formulario no es válido');
+    }
+  }
 }
